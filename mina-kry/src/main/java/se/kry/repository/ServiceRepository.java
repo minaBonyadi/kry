690233e3ab3f23dd13bb428f4c5408181ac38a7b
Mina <60871656+minaBonyadi@users.noreply.github.com>
2021-09-24 23:03:34 +0330

    Delete mina-kry directory

--- mina-kry/src/main/java/se/kry/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package se.kry.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import se.kry.model.ServiceIdentity;
-
-@Repository
-public interface ServiceRepository extends JpaRepository<ServiceIdentity, Long> {
-}
